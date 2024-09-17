a1ba364b2029355cc6dcc3a5101f4525e779f057
Nimesh Mandal <128793006+neeradian@users.noreply.github.com>
2024-09-17 09:29:50 +0530

    Example 4 Added

---
+++ CommandLine.java
@@ -0,0 +1,12 @@
+/**
+ * CommandLine
+ */
+public class CommandLine {
+
+    public static void main(String[] args) {
+        System.out.println("The command-line arguments are: ");
+        for (int x = 0; x < args.length; x++) {
+            System.out.println("args[" + x + "]:" + args[x]);
+        }
+    }
+}
