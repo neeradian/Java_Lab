6a4a83736122d2840e3d26ea412163cc408011fb
Nimesh Mandal <128793006+neeradian@users.noreply.github.com>
2024-09-17 09:41:24 +0530

    Experiment 4 Updated

--- CommandLine.java
+++ CommandLine.java
@@ -7,6 +7,7 @@ public class CommandLine {
         System.out.println("The command-line arguments are: ");
         for (int x = 0; x < args.length; x++) {
             System.out.println("args[" + x + "]:" + args[x]);
+            
         }
     }
 }
