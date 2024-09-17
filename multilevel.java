d468f3f70d1b75fd969f36f2775c67f75db24e1c
Nimesh Mandal <128793006+neeradian@users.noreply.github.com>
2024-09-17 09:26:06 +0530

    Example 3 Added

---
+++ multilevel.java
@@ -0,0 +1,31 @@
+class one {
+    public void print_geek(){
+        System.out.println("Geeks");
+    }
+}
+
+class two extends one{
+    public void  print_for(){
+        System.out.println("for");
+    }
+}
+
+class three extends two{
+    public void print_my(){
+        System.out.println("my");
+    }
+}
+
+/**
+ * Main
+ */
+public class multilevel {
+
+    public static void main(String[] args) {
+        three g=new three();
+
+        g.print_geek();
+        g.print_for();
+        g.print_my();
+    }
+}
