d34a73256a16327614cdce118dc5bbe21cf9357a
Nimesh Mandal <128793006+neeradian@users.noreply.github.com>
2024-09-17 09:16:18 +0530

    Create STtest.java
    Experiment 1 Added

---
+++ STtest.java
@@ -0,0 +1,36 @@
+
+/**
+ * STtest
+ */
+
+//import java.io.*;
+
+class Student {
+    private String name, city;
+    private int age;
+
+    public void getData(String x, String y, int t) {
+        name = x;
+        city = y;
+        age = t;
+    }
+
+    public void printData() {
+        System.out.println("Student name = " + name);
+        System.out.println("Student city = " + city);
+        System.out.println("Student age = " + age);
+    }
+}
+
+class STtest {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+        s1.getData("Kapil", "Delhi", 23);
+        s1.printData();
+
+        s2.getData("Amit", "Dehradun", 22);
+        s2.printData();
+    }
+}
