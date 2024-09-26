a18dccdb099ccb9b7de2e6f16840f4a9565df0fc
Nimesh Mandal <128793006+neeradian@users.noreply.github.com>
2024-09-26 14:51:48 +0530

    Updated Genric Method Test file

--- GenericMethodTest.java
+++ GenericMethodTest.java
@@ -1,26 +1,29 @@
-public class GenericMethodTest {
-    //generic method printArray
-    public static void printArray(E[] inputArray) 
+public class GenericMethodTest
+{
+   // generic method printArray                         
+   public static < E > void printArray( E[] inputArray )
+   {
+      // Display array elements              
+         for ( E element : inputArray ){        
+            System.out.printf( "%s ", element );
+         }
+         System.out.println();
+    }
+
+    public static void main( String args[] )
     {
-        //Display array elements
-        for (E[] element:inputArray)
-        {
-            System.out.printf("%s", element);
-        }
-        System.out.println();
+        // Create arrays of Integer, Double and Character
+        Integer[] intArray = { 1, 2, 3, 4, 5 };
+        Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+        Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
 
-    }
-    public static void main(String[] args) {
-        //Create array of integer, Double and Character Integer[]
-        int[] intArray = {1, 2, 3, 4, 5 };
-        double[] doubleArray = {1.1,2.2,3.3,4.4 };
-        char[] charArray = {'H','E','L','L','O'};
-        System.out.println("Array integerArray contains: ");
-        printArray(intArray);
-        System.out.println("\nArray doubleArray contains: ");
-        printArray(doubleArray);
-        System.out.println("\nArray CharacterArray contains: ");
-        printArray(charArray);
+        System.out.println( "Array integerArray contains:" );
+        GenericMethodTest.<Integer>printArray( intArray  ); // pass an Integer array
 
-    }
+        System.out.println( "\nArray doubleArray contains:" );
+        GenericMethodTest.<Double>printArray( doubleArray ); // pass a Double array
+
+        System.out.println( "\nArray characterArray contains:" );
+        GenericMethodTest.<Character>printArray( charArray ); // pass a Character array
+    } 
 }
